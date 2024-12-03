1fb2d8a4205d72bb4bcc61ef2ad2eaea3511ba25
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-12-03 17:57:43 +0100

    begin first challenge of day1

--- day1/day1.c
+++ day1/day1.c
@@ -1,6 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int	*parse_first_list(char *input)
+{
+	int	*first_list = calloc(sizeof(int), 1000);
+	int	i = 0;
+
+	if (!first_list)
+		return (NULL);
+	while (i < 1000)
+	{
+		first_list[i] = atoi(input);
+		while (*input != '\n' || *input != '\0')
+			printf("%c", *(input++));
+		input++;
+		printf("%d\n", i);
+		i++;
+	}
+	return (first_list);
+}
+
 int	main(int argc, char **argv)
 {
-	int	total_sum;
+	int	*first_list;
+	int	*second_list;
+	int	total_sum = 0;
+
+	if (argc != 2)
+		return (-1);
+
+	first_list = parse_first_list(argv[1]);
+	for	(int i = 0; i < 1000; i++)
+		printf("%d\n", first_list[i]);
 
 	// Parse input
 	//
@@ -8,5 +39,7 @@ int	main(int argc, char **argv)
 	//
 	// Count the differences
 
-	return (total_sum);
+	printf("Total difference between the lists is : %d\n", total_sum);
+
+	return (0);
 }
