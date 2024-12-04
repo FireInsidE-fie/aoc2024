e3d9ab84adf27c8ceb9229d33426f9502a4969d6
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-12-04 13:46:51 +0100

    add second list parsing

--- day1/day1.c
+++ day1/day1.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -19,6 +20,27 @@ int	*parse_first_list(char *input)
 	return (first_list);
 }
 
+int	*parse_second_list(char *input)
+{
+	int	*second_list = calloc(sizeof(int), 1000);
+	int	i = 0;
+
+	if (!second_list)
+		return (NULL);
+	while (i < 1000) 
+	{
+		while (isdigit(*input))
+			input++;
+		while (*input == ' ')
+			input++;
+		second_list[i++] = atoi(input);
+		while (*input && *input != '\n')
+			input++;
+		input++;
+	}
+	return (second_list);
+}
+
 int	main(int argc, char **argv)
 {
 	int	*first_list;
@@ -29,8 +51,9 @@ int	main(int argc, char **argv)
 		return (-1);
 
 	first_list = parse_first_list(argv[1]);
+	second_list = parse_second_list(argv[1]);
 	for	(int i = 0; i < 1000; i++)
-		printf("%d\n", first_list[i]);
+		printf("%d\t%d\n", first_list[i], second_list[i]);
 
 	// Parse input
 	//
@@ -41,7 +64,7 @@ int	main(int argc, char **argv)
 	printf("Total difference between the lists is : %d\n", total_sum);
 
 	free(first_list);
-	//free(second_list);
+	free(second_list);
 
 	return (0);
 }
